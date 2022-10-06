fa71f28ba604395fb500b62046e04378439ed711
Bodyshots <masterofbodyshots@gmail.com>
2022-10-06 12:49:29 -0400

    Edited __str__ to be center-aligned + added some comments + added get_entry function

--- test.py
+++ test.py
@@ -1,43 +1,52 @@
-def cons_pascal(row_num):
-    if row_num == 0:
-        return [1]
-    if row_num == 1:
-        return [1, 1] # will always be [1, x, 1], where x is a certain # of elems
-    
-    # rows >= 2
-    curr_row = [1]
-    prev_row = cons_pascal(row_num - 1)
-    for width in range(1, (row_num + 2) // 2):
-        curr_row.append(prev_row[width - 1] + prev_row[width])
+class PascalTriangle:
+    def __init__(self, height):
+        self.height = height
+        self.rows = [self.get_row(i) for i in range(height)]
 
-    # since pascal's triangle is symmetric, just copy curr_row reversed
-    # note: depending on the triangle's row number, we may have two middle elements
-    # so, we check parity of row_num:
-    # even: no duplicate mid elem
-    # odd: duplicate mid elem
-    if row_num % 2 == 0: # checks parity
-        curr_row.extend(curr_row[::-1][1:]) # don't dup the mid elem
-    else:
-        curr_row.extend(curr_row[::-1][:]) # dup the mid elem
-    return curr_row
+    def __str__(self):
+        # Meru Prastaara ver (left-aligned)
+        # result_str = "["
+        # for i in self.rows: result_str += str(i) + "\n"
+        # return result_str.rstrip("\n") + "]"
 
-def pascal_sum(row_num):
-    return sum(cons_pascal(row_num))
+        # Pascal Triangle ver
+        from os import get_terminal_size
+        term_width = get_terminal_size().columns
+        result_str = ""
+        for i in self.rows: result_str += str(i).center(term_width) + "\n"
+        return result_str.rstrip("\n")
 
-    
-    #recursive case
-    # will need to append <rows> times
-    """
-    Eg visualizaton:
-    0    [[1],
-    1    [1, 1],
-    2    [1, 2, 1],
-    3    [1, 3, 3, 1],
-    4    [1, 4, 6, 4, 1],
-    5    [1, 5, 10, 10, 5, 1],
-    6    [1, 6, 15, 20, 15, 6, 1]]
-    """
+    def get_row(self, row_num): # complexity: O(n!)?
+        if row_num < 0 or row_num > self.height:
+            return -1
+        if row_num == 0:
+            return [1]
+        if row_num == 1:
+            return [1, 1] # will always be [1, x, 1], where x is a certain # of elems
+        
+        # rows >= 2
+        curr_row = [1]
+        prev_row = self.get_row(row_num - 1)
+        for width in range(1, (row_num + 2) // 2):
+            curr_row.append(prev_row[width - 1] + prev_row[width])
 
-if __name__ == "__main__":
-    print(cons_pascal(10))
-    print(pascal_sum(10))
+        # since pascal's triangle is symmetric, just copy curr_row reversed
+        # note: depending on the triangle's row number, we may have two middle elements
+        # so, we check parity of row_num:
+        # even: no duplicate mid elem
+        # odd: duplicate mid elem
+        if row_num % 2 == 0: # checks parity
+            curr_row.extend(curr_row[::-1][1:]) # don't dup the mid elem
+        else:
+            curr_row.extend(curr_row[::-1][:]) # dup the mid elem
+        return curr_row
+
+    def pascal_sum(self, row_num): # complexity: O(1) ez
+        return 2 ** row_num
+        # alt: return sum(self.get_row(row_num))
+
+    def get_entry(self, row_num, col_num):
+        if ((row_num < 0 or row_num > self.height) or 
+            (len(self.rows[row_num]) < col_num or col_num < 0)):
+            return -1
+        return self.rows[row_num][col_num]
