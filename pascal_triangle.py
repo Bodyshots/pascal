65a4d568f15544c45bc8b2fa775d471865027586
Bodyshots <masterofbodyshots@gmail.com>
2022-10-07 01:27:13 -0400

    [PascalTriangleBST] implemented logic for the class, namely get_row and PascalTriangle's initializer. also attempted to implement PascalTriangle's __str__ method (not correct though). i need sleep

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -19,9 +19,14 @@ class Queue:
         """
         return self._elems[index]
 
+    def is_empty(self): return len(self) == 0
+
+    def empty_queue(self):
+        for i in range(len(self._elems)): self.dequeue()
+
 
 class PascalBST:
-    def __init__(self, elem=None, left=None, right=None, parent=None):
+    def __init__(self, elem=None, left=None, right=None):
         if (elem is None):
             self.left = None
             self.right = None
@@ -30,46 +35,117 @@ class PascalBST:
             self.right = PascalBST(right)
 
         self.root = elem
-        self.p = parent
-        self.colour = 1 # 1 => White, 0 => Black (visited)
-
+        self.colour = 0 # white => 1, black => 0
+    
+    def __str__(self): return str(self.root)
 
 class PascalTriangle:
     def __init__(self, row_num):
         if row_num >= 0:
             self.top = PascalBST(1)
+            self.vertices = [self.top] # note: technically, self.vertices can basically represent
+                                       # pascal's triangle itself, but we'll just ignore that for
+                                       # the purposes of making it via BSTs/Graphs 
+            if row_num == 0: return
         if row_num >= 1:
             self.top.left = PascalBST(1)
             self.top.right = PascalBST(1)
+            self.vertices = [self.top, self.top.left, self.top.right]
+            if row_num == 1: return
 
+        self.vertices = [self.top, self.top.left, self.top.right]
+        curr_left = self.top.left
+        curr_right = self.top.right
 
-        for i in range(row_num):
-            row_entries = [PascalBST(1, 1)]
-            row_entries.extend([PascalBST() for _ in range(j - 1)])
-            row_entries.append(PascalBST(1, None, 1))
+        curr_row_queue = Queue()
+        next_row_queue = Queue()
+        upcoming_row = Queue()
+        for i in range(1, row_num):
+            # get the nodes for the next lvl
+            next_row = self.get_row(i + 1)
+            for node in next_row[1: len(next_row) - 1]: 
+                next_row_queue.enqueue(node)
+                self.vertices.append(node)
 
-            prev_row = self.cons_pascal(i - 1)
-            for node in row_entries[1: len(row_entries) - 1]:
-                new_entry = prev_row[width - 1] + prev_row[width]
+            # leftmost col is always 1s
+            curr_left.left = PascalBST(1)
+            upcoming_row.enqueue(curr_left.left)
 
-        # if row_num == 0:
-        #     return PascalBST(1)
-        # if row_num == 1:
-        #     return PascalBST(1, 1, 1)
+            while not (next_row_queue.is_empty() or curr_row_queue.is_empty()):
+                next_node = next_row_queue.dequeue()
+                upcoming_row.enqueue(next_node)
+
+                curr_left.right, curr_right.left = next_node, next_node
+
+                curr_left = curr_right
+                curr_right = curr_row_queue.dequeue()
+
+            # rightmost col is always 1s
+            curr_right.right = PascalBST(1)
+            if (curr_row_queue.is_empty() and not next_row_queue.is_empty()):
+                next_node = next_row_queue.dequeue()
+                curr_left.right, curr_right.left = next_node, next_node
+            upcoming_row.enqueue(curr_right.right)
         
-        # # rows >= 2
-        # curr_row = [1]
-        # prev_row = self.cons_pascal(row_num - 1)
-        # for width in range(1, (row_num + 2) // 2):
-        #     curr_row.append(prev_row[width - 1] + prev_row[width])
+            curr_row_queue = upcoming_row
+            curr_left = curr_row_queue.dequeue()
+            curr_right = curr_row_queue.dequeue()
+            upcoming_row.empty_queue()
 
-        # # since pascal's triangle is symmetric, just copy curr_row reversed
-        # # note: depending on the triangle's row number, we may have two middle elements
-        # # so, we check parity of row_num:
-        # # even: no duplicate mid elem
-        # # odd: duplicate mid elem
-        # if row_num % 2 == 0: # checks parity
-        #     curr_row.extend(curr_row[::-1][1:]) # don't dup the mid elem
-        # else:
-        #     curr_row.extend(curr_row[::-1][:]) # dup the mid elem
-        # return curr_row
+    def __str__(self):
+        from os import get_terminal_size
+        # basically, BFS traversal, starting from self.top
+        for i in self.vertices: i.colour = 0
+
+        nodes_in_lvl = 0
+        row_num = 0
+        result_str = ""
+        queue = Queue()
+        self.top.colour = 1
+        term_width = get_terminal_size().columns
+        queue.enqueue(self.top)
+        while not queue.is_empty():
+            result_str += "\n"
+            curr_node = queue.dequeue()
+
+            nodes_in_lvl += 1
+            if row_num + 1 == nodes_in_lvl:
+                result_str += f"{str(curr_node.root)}\n".center(term_width)
+                # prep for next lvl
+                nodes_in_lvl = 0
+                row_num += 1
+
+            else: result_str += f"{str(curr_node.root)}  ".center(term_width)
+            if curr_node.left and curr_node.left.colour == 0:
+                if (curr_node.left.root):
+                    curr_node.left.colour = 1
+                    queue.enqueue(curr_node.left)
+            if curr_node.right.colour == 0:
+                if (curr_node.right.root):
+                    curr_node.right.colour = 1
+                    queue.enqueue(curr_node.right)
+            curr_node.colour = 1
+        return result_str
+
+    def get_row(self, row_num):
+        if row_num == 0:
+            return [PascalBST(1)]
+        if row_num == 1:
+            return [PascalBST(1), PascalBST(1)]
+
+        # row_num >= 2
+        row_entries = [PascalBST(1)]
+        row_entries.extend([PascalBST() for _ in range(row_num - 1)])
+        row_entries.append(PascalBST(1))
+        
+        prev_row = self.get_row(row_num - 1)
+        for node in range(1, len(row_entries) - 1):
+            row_entries[node].root = prev_row[node - 1].root + prev_row[node].root
+            row_entries[node].left = PascalBST(None)
+            row_entries[node].right = PascalBST(None)
+
+        return row_entries
+
+if __name__ == "__main__":
+    test = PascalTriangle(6)
+    print(test)
